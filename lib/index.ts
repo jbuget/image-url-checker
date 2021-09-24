1d6de23ec66d3df4cdcdce14d1472833054b049c
jbuget <jbuget@gmail.com>
2021-09-24 21:15:42 +0200

    Add commander lib

--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,15 @@
-console.log('It works !');
+import { Command } from 'commander';
+const program = new Command();
+program.version('0.0.1');
+
+program
+  .option('-v --verbose', 'display details')
+  .option('-o --output', 'output file');
+
+program.parse(process.argv);
+
+const options = program.opts();
+
+if (options.verbose) console.log(options);
+
+
