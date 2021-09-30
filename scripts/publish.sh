f79194e7d836e35f131021a867a139b379acfc24
jbuget <jbuget@gmail.com>
2021-10-01 00:35:53 +0200

    Improve release publication

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,8 +6,8 @@ echo "${version_type}"
 npm run package
 git add dist
 git commit -m 'Package a new binary'
-npm version "${version_type}"
-npm publish
+npm version "${version_type}" -m "Upgrade to version %s"
 git push
 git push --tags
+npm publish
 
