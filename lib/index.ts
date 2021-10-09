014ca80080f8f9287b8fd0505180ed91087f2b0a
jbuget <jbuget@gmail.com>
2021-10-09 18:20:35 +0200

    Make core plugin registered by default and image plugin declared as a tierce plugin

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 
 import Program from './Program';
+import { plugin as imagesPlugin } from './plugins/images/ImagesPlugin';
 
 const program = new Program();
+
+program.use(imagesPlugin);
 
 program.run(process.argv).then(() => process.exit(0));
